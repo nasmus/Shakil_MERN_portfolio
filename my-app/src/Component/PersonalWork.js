324e6630fbc40ddd27be99e79763fd6f65c8059a
nasmus <nasmus.shahadat@gmail.com>
2023-05-21 20:11:08 +0600

    work

--- my-app/src/Component/PersonalWork.js
+++ my-app/src/Component/PersonalWork.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../CSS/PersonalWork.css';
+import { Link } from 'react-router-dom';
 
 function PersonalWork() {
   return (
     <div className='personal__work'>
         <div data-aos="fade-up" className='personal__work__container' style={{paddingLeft:'30px'}} >
-            <h1>Personal Project</h1>
-            <div className='personal__work__content' >
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </div>
+          <h1>Personal Project</h1>
+            <div className="portfolioPage__body" data-aos="fade-down-left" >
+              <div className="portfolioPage__title">
+                <h3></h3>
+              </div>   
+            </div> 
         </div>
     </div>
   )
--- my-app/src/Component/SkillsComponent.js
+++ my-app/src/Component/SkillsComponent.js
@@ -1,52 +1,65 @@
-import React from 'react'
-import '../CSS/SkillComponent.css'
+import React from "react";
+import "../CSS/SkillComponent.css";
 
 function SkillsComponent() {
+  const techStack = ["React.js", "Express JS", "Redux", "Node.js"];
+  const languages = ["React.js", "Express JS", "Redux", "Node.js"];
+  const backendStack = ["React.js", "Express JS", "Redux", "Node.js"];
   return (
     <div>
-        <div className='Right_component__skills' >
-          <div style={{paddingLeft:'30px'}} data-aos="fade-up" >
+      <div className="Right_component__skills">
+        <div style={{ paddingLeft: "30px" }} data-aos="fade-up">
           <h1>Skills</h1>
-            <div className='Right_component_skills__component' >
-              <div className='Right_component_skills__Left' >
-                <ul style={{paddingBottom:'20px'}}>
-                  <h3>
+          <div className="Right_component_skills__component">
+            <div className="Right_component_skills__Left">
+              <ul style={{ paddingBottom: "20px" }}>
+                <h3>
                   <li>FreamWork</li>
-                  </h3>
-                  <ul>
-                    <h4><li>React.js</li></h4> 
-                    <h4><li>Express.js</li></h4> 
-                    <h4><li>Redux</li></h4> 
-                    <h4><li>Node.js</li></h4> 
-                  </ul>
-                </ul>
-
+                </h3>
                 <ul>
-                  <h3>
-                  <li>Language</li>
-                  </h3>
-                  <ul>
-                    <h4><li>JavaScript</li></h4> 
-                    <h4><li>Python</li></h4> 
-                    <h4><li>HTML5</li></h4>
-                    <h4><li>CSS</li></h4> 
-                  </ul>
+                  {techStack.map((item, index) => (
+                    <li key={index + "li"}>
+                      <h4>{item}</h4>
+                    </li>
+                  ))}
                 </ul>
-              </div>
-              <div className='Right_component_skills__right' >
+              </ul>
+
               <ul>
-                <h4><li>MongoDb</li></h4>
-                <h4><li>JWT Token</li></h4>
-                <h4><li>JSX</li></h4>
-                <h4><li>Context Api</ li></h4>
+                <h3>
+                  <li>Language</li>
+                </h3>
+                <ul>
+                  <h4>
+                    <li>JavaScript</li>
+                  </h4>
+                  <h4>
+                    <li>Python</li>
+                  </h4>
+                  <h4>
+                    <li>HTML5</li>
+                  </h4>
+                  <h4>
+                    <li>CSS</li>
+                  </h4>
+                </ul>
               </ul>
             </div>
+            <div className="Right_component_skills__right">
+
+              <ul>
+                {backendStack.map((item, index) => (
+                  <li key={index + "li"}>
+                    <h4>{item}</h4>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-            
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SkillsComponent
+export default SkillsComponent;
