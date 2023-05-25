e4bf8a67596aaa3c3faef3c776f34763c48de70c
nasmus <nasmus.shahadat@gmail.com>
2023-05-25 08:10:48 +0600

    last commit

--- my-app/src/Component/TestAchievement.js
+++ my-app/src/Component/TestAchievement.js
@@ -20,8 +20,28 @@ function TestAchievement() {
               <div className='container Right-container'>
               <img src={aiub} />
                 <div className='text-box'>
-                  <h2>AIUB</h2>
+                  <h2>Programing Contest</h2>
+                  <small>2018</small>
+                  <p>Appreciation for participation in event</p>
+                  <span className='Right-container-arrow' ></span>
+                </div>                
+              </div>
+
+              {/* <div className='container Left-container'>
+              <img src={google} />
+                <div className='text-box'>
+                  <h2>Aiub</h2>
                   <small>2018 - 2019</small>
+                  <p>this is the p tag for discription</p>
+                  <span className='Left-container-arrow' ></span>
+                </div>                
+              </div>
+
+              <div className='container Right-container'>
+              <img src={google} />
+                <div className='text-box'>
+                  <h2>Programing Contest</h2>
+                  <small>2018</small>
                   <p>this is the p tag for discription</p>
                   <span className='Right-container-arrow' ></span>
                 </div>                
@@ -45,27 +65,7 @@ function TestAchievement() {
                   <p>this is the p tag for discription</p>
                   <span className='Right-container-arrow' ></span>
                 </div>                
-              </div>
-
-              <div className='container Left-container'>
-              <img src={google} />
-                <div className='text-box'>
-                  <h2>Aiub</h2>
-                  <small>2018 - 2019</small>
-                  <p>this is the p tag for discription</p>
-                  <span className='Left-container-arrow' ></span>
-                </div>                
-              </div>
-
-              <div className='container Right-container'>
-              <img src={google} />
-                <div className='text-box'>
-                  <h2>Aiub</h2>
-                  <small>2018 - 2019</small>
-                  <p>this is the p tag for discription</p>
-                  <span className='Right-container-arrow' ></span>
-                </div>                
-              </div>
+              </div> */}
         </div>
     </div>
   )
